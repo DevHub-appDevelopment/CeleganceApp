f2449f6b6577e4bd6a15fea30df147d0fd3eb1b6
dai-hsekar-sama <swain.rakesh131@gmail.com>
2018-02-28 12:29:10 +0530

    New Activity Added

---
+++ app/src/main/java/com/example/sujit/celeganceapp/AdminPanel.java
@@ -0,0 +1,54 @@
+package com.example.sujit.celeganceapp;
+
+import android.content.Intent;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by user on 2/27/2018.
+ */
+
+public class AdminPanel extends AppCompatActivity {
+    FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_panel);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null)
+        {
+            Intent intent = new Intent(AdminPanel.this,AdminAuth.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+}
